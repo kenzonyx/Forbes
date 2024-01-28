0f03485de37ace6f72852d8840be8fcff08e3df9
Onyemulu Honour <onyemuluhonour@gmail.com>
2024-01-28 19:08:51 +0100

    Image Changes

--- src/ForbesData.js
+++ src/ForbesData.js
@@ -1,42 +1,42 @@
 const ForbesData = [
   {
     title: 'Elon Musk',
-    imageSrc: '/public/Elon-musk.jpg',
+    imageSrc: '/Elon-musk.jpg',
     description: 'Card Description goes here...',
   },
   {
     title: 'Bernard Arnault',
-    imageSrc: '/public/Bernard Arnault.jpg',
+    imageSrc: '/Bernard Arnault.jpg',
     description: 'Card Description goes here...',
   },
   {
     title: 'Bill Gate',
-    imageSrc: '/public/Bill gate.jpg',
+    imageSrc: '/Bill gate.jpg',
     description: 'Card Description goes here...',
   },
   {
     title: 'Jeff Bezos',
-    imageSrc: '/public/jeff-Bezos.jpg',
+    imageSrc: '/jeff-Bezos.jpg',
     description: 'Card Description goes here...',
   },
   {
     title: 'Larry Ellison',
-    imageSrc: '/public/Larry_Ellison.png',
+    imageSrc: '/Larry_Ellison.png',
     description: 'Card Description goes here...',
   },
   {
     title: 'Larry Page',
-    imageSrc: '/public/Larry_Page.jpg',
+    imageSrc: '/Larry_Page.jpg',
     description: 'Card Description goes here...',
   },
   {
     title: 'Mark Zuckerberg',
-    imageSrc: '/public/Mark zuckerberg.jpg',
+    imageSrc: '/Mark zuckerberg.jpg',
     description: 'Card Description goes here...',
   },
   {
     title: 'Warren Bufffet',
-    imageSrc: '/public/Warren-Buffet.jpg',
+    imageSrc: '/Warren-Buffet.jpg',
     description: 'Card Description goes here...',
   },
 ];
